6085b67231b4c66e5f17fd874044587a11f16d6a
Carlos <cdamota.cd@gmail.com>
2026-07-28 16:50:23 +0200

    feat(i18n): add Catalan locale fields to categories and products

--- db/migrations/003-add-catalan-fields.sql
+++
@@ -1,9 +0,0 @@
--- Add Catalan (ca) locale fields to categories and products
--- This migration adds name_ca and description_ca columns to support Catalan language
-
--- Categories: add name_ca
-ALTER TABLE categories ADD COLUMN name_ca TEXT NOT NULL DEFAULT '';
-
--- Products: add name_ca and description_ca
-ALTER TABLE products ADD COLUMN name_ca TEXT NOT NULL DEFAULT '';
-ALTER TABLE products ADD COLUMN description_ca TEXT NOT NULL DEFAULT '';
