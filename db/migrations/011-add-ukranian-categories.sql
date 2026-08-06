e6ffa16171d4756eb53e8118b4a6fd86db3ff342
Fran <fcojmm91@gmail.com>
2026-08-07 01:04:42 +0200

    Fix: Añadida migracion para actualizar categorias en Ukr. Cambiado Entrantes por Picoteo y derivados. Añadido Altres para otros en catalan

---
+++ db/migrations/011-add-ukranian-categories.sql
@@ -0,0 +1,20 @@
+BEGIN TRANSACTION;
+
+--Migracion para insertar la traducción de las categorías al ukraniano
+
+UPDATE categories SET name_uk = 'меню' WHERE slug = 'menus';
+UPDATE categories SET name_uk = 'Основні страви' WHERE slug = 'platos';
+UPDATE categories SET name_uk = 'Клювання' WHERE slug = 'entrantes';
+UPDATE categories SET name_uk = 'Напої' WHERE slug = 'bebidas';
+UPDATE categories SET name_uk = 'Десерти' WHERE slug = 'postres';
+UPDATE categories SET name_uk = 'Інші' WHERE slug = 'otros';
+
+--Y actualizo los entrantes a Picoteo / Pica-pica
+
+UPDATE categories SET name_es = 'Picoteo' WHERE slug = 'entrantes';
+UPDATE categories SET name_ca = 'Pica-pica' WHERE slug = 'entrantes';
+
+--Y Otros en catalan
+UPDATE categories SET name_ca = 'Altres' WHERE slug = 'otros';
+
+COMMIT;
